75d9b5e99f485a7d77f950180f9e59b16a45cc81
Tyler White <twigwhite@gmail.com>
2019-06-08 15:40:58 -0500

    Camera controller stuff, game manager with ship death events

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿public static class GameManager
+{
+    private static Ship _ship;
+
+    public static void SetShip(Ship ship)
+    {
+        _ship = ship;
+    }
+
+    public static event System.Action OnShipDeath;
+
+    public static void FireShipDeath()
+    {
+        OnShipDeath?.Invoke();
+    }
+}
