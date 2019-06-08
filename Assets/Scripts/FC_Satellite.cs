f52194229b45cca9048dba644357ba7ccceb8d28
fpcatlett <fpcatlett@gmail.com>
2019-06-08 16:02:57 -0500

    added satellite object and updated forrest test scene

---
+++ Assets/Scripts/FC_Satellite.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FC_Satellite : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip whiteNoise;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = whiteNoise;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GetComponent<AudioSource>().Stop();
+    }
+}
