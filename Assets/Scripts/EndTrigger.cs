80d99959409c34f3495af65b4f8d0897900bb603
Tyler White <twigwhite@gmail.com>
2019-06-08 17:10:17 -0500

    Post game menu

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class EndTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _postGameScreen;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var ship = other.gameObject.GetComponent<Ship>();
+        if (ship != null)
+        {
+            GameManager.FireWinTriggerHit();
+            Instantiate(_postGameScreen);
+        }
+    }
+}
