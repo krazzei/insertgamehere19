0996de6b4844854cb8700ac8546c037d6090bae9
Tyler White <twigwhite@gmail.com>
2019-06-08 14:06:07 -0500

    Start of ship code

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D), typeof(Rigidbody2D))]
+public class Ship : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    [SerializeField]
+    private GameObject _leftThruster;
+
+    [SerializeField]
+    private GameObject _rightThruster;
+
+    [SerializeField]
+    private float thrustForce;
+    
+    private Transform _leftTransform;
+    
+    private Transform _rightTransform;
+
+    [SerializeField]
+    private Transform _leftSide;
+
+    [SerializeField]
+    private Transform _rightSide;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _leftTransform = _leftThruster.transform;
+        _rightTransform = _rightThruster.transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.AddForceAtPosition(_leftTransform.up * thrustForce, _leftTransform.position);
+            _rigidbody.AddForceAtPosition(_rightTransform.up * thrustForce, _rightTransform.position);
+        }
+        
+        // TODO: might want and Input controller of some sort?
+        // TODO: at the least we need to use GetAxis or GetAction to use the arcade stick.
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _rigidbody.AddForceAtPosition(_leftTransform.up * thrustForce, _leftTransform.position);
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            _rigidbody.AddForceAtPosition(_rightTransform.up * thrustForce, _rightTransform.position);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _rigidbody.AddForceAtPosition(thrustForce * 0.5f * _leftSide.right, _leftSide.position);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            _rigidbody.AddForceAtPosition(thrustForce * -0.5f * _rightSide.right, _rightSide.position);
+        }
+    }
+}
---
+++ Assets/Scripts/SimpleFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    private Transform _transform;
+    private Vector3 _targetPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _targetPos = _target.position;
+        _targetPos.z = -10;
+        _transform.position = _targetPos;
+    }
+}
