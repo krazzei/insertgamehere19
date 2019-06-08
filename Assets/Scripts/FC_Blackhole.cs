956f151faa632bd3feb517a19c4cbfab6294f291
fpcatlett <fpcatlett@gmail.com>
2019-06-08 16:33:58 -0500

    created blackhole object

---
+++ Assets/Scripts/FC_Blackhole.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FC_Blackhole : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip blackHole;
+    [SerializeField]
+    AudioSource music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = blackHole;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<AudioSource>().Play();
+        music.mute = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GetComponent<AudioSource>().Stop();
+        music.mute = false;
+    }
+}
