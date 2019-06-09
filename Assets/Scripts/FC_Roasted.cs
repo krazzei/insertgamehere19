e10bf38c7db396e02760be8a1b4112c3418a6ee8
fpcatlett <fpcatlett@gmail.com>
2019-06-08 19:09:50 -0500

    added roastcup power up and updated forrest test scene

---
+++ Assets/Scripts/FC_Roasted.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FC_Roasted : MonoBehaviour
+{
+    [SerializeField]
+    SpriteRenderer roast;
+    [SerializeField]
+    AudioClip ohYeah;
+    [SerializeField]
+    AudioSource roastSong;
+    [SerializeField]
+    AudioSource currentSong;
+
+    [SerializeField]
+    float roastDuration = 15.0f;
+    float timer = 0.0f;
+    bool startRoast = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = ohYeah;
+        roast = GetComponent<SpriteRenderer>();
+        roast.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Roasting();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<AudioSource>().Play();
+        roast.enabled = false;
+        currentSong.Pause();
+        roastSong.Play();
+        startRoast = true;
+ 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+
+    void Roasting()
+    {
+        if (startRoast == true)
+        {
+            Destroy(gameObject, roastDuration);
+            timer += Time.deltaTime;
+            if (timer > roastDuration)
+            {
+                currentSong.Play();
+                roastSong.Pause();
+                timer = 0.0f;
+                startRoast = false;
+            }
+        }
+    }
+}
